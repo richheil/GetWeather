88f44316adba8c3f4ad779eb8755b55a5612a951
richheil <richardwheil@gmail.com>
2017-06-11 19:46:26 -0400

    made cosmetic updates to move things not in the function into the main

--- getweather.py
+++ getweather.py
@@ -2,20 +2,6 @@
 
 import requests,json
 
-
-api_key='cbc6d2ae9bbbce47aad5c0c96d20a4af' 
-url='http://api.openweathermap.org/data/2.5/weather'#?zip='+zipcode+'&units=imperial&appid='+api_key
-
-#get user zipcode
-while True:
-    zipcode = raw_input("Please enter zipcode to check weather(default Los Angeles): ") or "90001"
-    if not zipcode.isdigit() or int(zipcode)/100000 !=0:
-        print ("That does not look like a zipcode please try again")
-        continue
-    else:
-        break
-
-
 def getWeather(url,zipcode,api_key):
 #Get api response for location given
     try:
@@ -38,8 +24,20 @@ def getWeather(url,zipcode,api_key):
 
     print "In %s it is %s%sF and %s with a low of %s and a high of %s. Humidity is %s%%." % (location, temp, degree, description, temp_min, temp_max, humidity)
 
-getWeather(url,zipcode,api_key)
+if __name__ == "__main__":
 
+#I would not usually include an api key in a public git repo, but I erred on the side of easy of use in this case
+    api_key='cbc6d2ae9bbbce47aad5c0c96d20a4af'
+    url='http://api.openweathermap.org/data/2.5/weather'
 
+#get user zipcode
+    while True:
+        zipcode = raw_input("Please enter zipcode to check weather(default Los Angeles): ") or "90001"
+        if not zipcode.isdigit() or int(zipcode)/100000 !=0:
+            print ("That does not look like a zipcode please try again")
+            continue
+        else:
+            break
 
-exit()
+    getWeather(url,zipcode,api_key)
+
